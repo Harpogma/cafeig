56bd610d577caaffdef8bddc24fd9e5a76500c25
Harpogma <gd.temp@pm.me>
2025-05-01 22:05:31 +0200

    frontend improvments

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,19 +1,19 @@
-<nav class="bg-gray-200 border-gray-200">
+<nav>
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="./../index.php">Café HEIG</a>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-200 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-gray-200">
+            <ul class="flex flex-col p-4 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
                 <li>
-                    <a href="./index.php" class="block py-2 pl-3 pr-4 text-gray-900 rounded md:bg-transparent md:p-0" aria-current="page">Home</a>
+                    <a href="./index.php" class="block py-2 pl-3 pr-4 text-gray-900 text-sm rounded md:bg-transparent md:p-0" aria-current="page">Home</a>
                 </li>
                 <li>
-                    <a href="./about.php" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0">À propos</a>
+                    <a href="./about.php" class="block py-2 pl-3 pr-4 text-gray-900 text-sm rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0">À propos</a>
                 </li>
                 <li>
-                    <a href="./register.php" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0">S'inscrire</a>
+                    <a href="./register.php" class="block py-2 pl-3 pr-4 text-gray-900 text-sm rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0">S'inscrire</a>
                 </li>
                 <li>
-                    <a href="./login.php" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0">Se connecter</a>
+                    <a href="./login.php" class="block py-2 pl-3 pr-4 text-gray-900 text-sm rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0">Se connecter</a>
                 </li>
             </ul>
         </div>
